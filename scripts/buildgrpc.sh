86e9b5046f144d46628f78b5fcf059d81918e384
edidada <sandisks555@gmail.com>
2020-05-29 13:29:05 +0800

    add gitlab

--- scripts/buildgrpc.sh
+++ scripts/buildgrpc.sh
@@ -4,9 +4,10 @@ export MY_INSTALL_DIR=$HOME/.local
 mkdir -p $MY_INSTALL_DIR
 export PATH="$PATH:$MY_INSTALL_DIR/bin"
 cmake --version
-sudo yum install -y build-essential autoconf libtool pkg-config
-git clone --recurse-submodules -b v1.28.1 https://github.com/grpc/grpc
+yum install -y build-essential autoconf libtool pkg-config
+git clone --recurse-submodules https://github.com/grpc/grpc
 cd grpc
+git checkout v1.28.1
 mkdir -p cmake/build
 cd cmake/build
 cmake -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR ../../
